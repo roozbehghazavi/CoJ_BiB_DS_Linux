2d91e16d3bfb5895084ab55bf324539eed4b5a91
roozbehghazavi <roozbehghazavi3@gmail.com>
2025-10-28 18:46:46 +0330

    Controller update to resolve bugs within wine

--- Scripts/launch_controller.sh
+++ Scripts/launch_controller.sh
@@ -3,6 +3,6 @@
 export WINEPREFIX=/root/wine-coj2
 export DISPLAY=:99
 
-nohup bash -c 'wine "/root/wine-coj2/drive_c/Program Files/CoJ2 Controller/CoJ2Controller.exe"' >/dev/null 2>&1 & disown
+nohup bash -c 'wine "/root/CoJ_BiB_DS_Linux/Applications/CoJ2 Controller/CoJ2Controller.exe"' >/dev/null 2>&1 & disown
 
 echo "✅ Controller started in background."
