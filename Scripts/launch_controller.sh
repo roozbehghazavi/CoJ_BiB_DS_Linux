c50ac49c339c00b5342efdb0f287f13f1bc3648d
Roozbeh <56149349+roozbehghazavi@users.noreply.github.com>
2026-08-10 12:32:09 +0200

    Set WINEDEBUG and WINEDLLOVERRIDES for wine
    Added WINEDEBUG and WINEDLLOVERRIDES environment variables.

--- Scripts/launch_controller.sh
+++ Scripts/launch_controller.sh
@@ -2,6 +2,8 @@
 
 export WINEPREFIX=/root/wine-coj2
 export DISPLAY=:99
+export WINEDEBUG=-all
+export WINEDLLOVERRIDES="version=n,b"
 
 nohup bash -c 'wine "/root/CoJ_BiB_DS_Linux/Applications/CoJ2 Controller/CoJ2Controller.exe"' >/dev/null 2>&1 & disown
 
